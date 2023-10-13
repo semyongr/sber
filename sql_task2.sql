618fdc32a1f0f31ebc80a6aa52854bb60bdba447
semyongr <102382481+semyongr@users.noreply.github.com>
2023-10-14 00:38:46 +0300

    Update sql_task2.sql

--- sql_task2.sql
+++ sql_task2.sql
@@ -1,7 +1,7 @@
 SELECT employee.id, employee.name, 
 COUNT(sales.price) AS sales_c,
-rank() over (order by COUNT(sales.price) desc) sales_rank_c,
+RANK() OVER (ORDER BY COUNT(sales.price) DESC) sales_rank_c,
 SUM(sales.price) AS sales_s,
-rank() over (order by SUM(sales.price) desc) sales_rank_s
+RANK() OVER (ORDER BY SUM(sales.price) DESC) sales_rank_s
 FROM employee INNER JOIN sales ON employee.id = sales.employee_id
 GROUP BY employee.id, employee.name
